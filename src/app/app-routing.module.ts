dc8a8dd720e23d56b4dc903705e74cc9269cc78a
Patrick Kersten <Patrick.Kersten@planonsoftware.com>
2019-08-08 15:47:54 +0200

    add pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MonthlyComponent } from './pages/monthly/monthly.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'monthly', component: MonthlyComponent },
+  { path: 'about', component: AboutComponent },
+  { path: '**', component: HomeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MonthlyComponent } from './pages/monthly/monthly.component';
+import { PageHeaderComponent } from './components/page-header/page-header.component';
+import { AboutComponent } from './pages/about/about.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MonthlyComponent,
+    PageHeaderComponent,
+    AboutComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
